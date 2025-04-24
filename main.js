15e4b1534983c9eb07225c8de154219d0a8fa150
Fabio-Moschella <fabiomoschella1999@gmail.com>
2025-04-24 12:01:34 +0200

    add-function-button

--- main.js
+++ main.js
@@ -1,19 +1,32 @@
 const apiUrl = "https://flynn.boolean.careers/exercises/api/random/mail";
 const emailList = document.getElementById("emailList");
 const fetchEmail = document.getElementById("fetchEmailList");
-const arreyEmail = [];
-console.log("PRIMA DI GET");
-for (let i = 0; i < 10; i++) {
-  axios.get(apiUrl).then((res) => {
-    arreyEmail.push(res.data.response);
-    if (arreyEmail.length === 10) {
-      console.log(arreyEmail);
-      for (let i = 0; i < arreyEmail.length; i++) {
-        console.log(arreyEmail[i]);
 
-        emailList.innerHTML += `<li>${arreyEmail[i]}</li>`;
+const numberEmail = 10;
+console.log("PRIMA DI GET");
+
+function generateEmail(numberEmail) {
+  const arreyEmail = [];
+  // emailList.innerHTML = "";
+  let emailListHtml = "";
+  for (let i = 0; i < numberEmail; i++) {
+    axios.get(apiUrl).then((res) => {
+      arreyEmail.push(res.data.response);
+      console.log(arreyEmail);
+      if (arreyEmail.length === 10) {
+        console.log(arreyEmail);
+        for (let i = 0; i < arreyEmail.length; i++) {
+          const currentEmail = arreyEmail[i];
+
+          emailListHtml += `<li>${currentEmail}</li>`;
+        }
+        emailList.innerHTML = emailListHtml;
       }
-    }
-  });
+    });
+  }
 }
 console.log("DOPO DEL GET");
+
+fetchEmail.addEventListener("click", () => {
+  generateEmail(numberEmail);
+});
